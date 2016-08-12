14868f930e6aa881ed9c457f3ef003d65b0f430b
Fernando Alfonso <feralvil@gmail.com>
2016-08-12 09:33:37 +0200

    Leer y Cargar Eventos vía web
    Pendiente de agregar equipos a mano

---
+++ app/webroot/eventos_leer.php
@@ -0,0 +1,78 @@
+<?php
+// Datos del fichero CSV;
+$fecha = date('Y-m-d');
+$ficherocsv = 'files/eventos_' . $fecha . '.csv';
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title><?php echo 'Importar datos CSV';?></title>
+        <link rel="StyleSheet" type="text/css" href="css/ink.css">
+    </head>
+    <body>
+        <div id="container" class="ink-grid">
+            <h1><?php echo 'Importar datos CSV';?></h1>
+            <?php
+            $fila = 1;
+            if (($puntero = fopen($ficherocsv, "r")) !== FALSE) {
+            ?>
+                <div class='content-center'>
+                    <a class="ink-button blue" href="eventos_update.php" title="Guardar Eventos">
+                        <i class = "icon-upload"></i> Guardar Eventos
+                    </a>
+                </div>
+                <table class="ink-table bordered alternating hover">
+                <?php
+                while ((($datos = fgetcsv($puntero, 1000, ",")) !== FALSE)) {
+                    $tag = 'td';
+                    if ($fila == 1){
+                        $tag = 'th';
+                    }
+                ?>
+                    <tr>
+                        <?php
+                        $dato = ($fila - 1);
+                        if ($fila == 1){
+                            $dato = "Fila";
+                        }
+                        echo '<' . $tag . '>' . $dato . '</' . $tag . '>';
+                        ?>
+
+                        <?php
+                        for ($i = 0; $i < count($datos) ; $i++){
+                            if (($fila > 1) && ($i > 4) && ($i < 7)){
+                                if ($datos[$i] != ''){
+                                    $vectordato = explode(' ', $datos[$i]);
+                                    $vectorfecha = explode('/', $vectordato[0]);
+                                    $fecha = "20" . $vectorfecha[2] . '-' . $vectorfecha[1] . '-' . $vectorfecha[0];
+                                    $hora = $vectordato[1];
+                                    $datos[$i] = $fecha . ' ' .$hora;
+                                }
+                            }
+                            echo '<' . $tag . '>' . utf8_encode ($datos[$i]) . '</' . $tag . '>';
+                        }
+                        ?>
+                    </tr>
+                <?php
+                    $fila++;
+                }
+                fclose($puntero);
+                ?>
+                </table>
+            <?php
+            }
+            else {
+            ?>
+                <div class="ink-alert block warning">
+                    <h4><?php echo 'Error en la búsqueda del Fichero CSV';?></h4>
+                    <p><?php echo 'No se han encontrado el fichero CSV' . ' ' . $ficherocsv; ?></p>
+                </div>
+            <?php
+            }
+            ?>
+
+        </div>
+    </body>
+</html>
+<?php
+?>
